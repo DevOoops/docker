fa0c0cceb0c81a90377d223468b876df50855f69
Kessler dev <kessler.dev@gmail.com>
2018-10-04 14:31:38 +0200

    be posix compliant

--- php/entrypoint-tideways.sh
+++ php/entrypoint-tideways.sh
@@ -8,7 +8,7 @@ if [ -f /run/secrets/tideways_api_key ]; then
     echo "Tideways secret found"
 fi
 # if secret(k8s) is set
-if [ -f /run/secrets/shared/tideways_api_key ] && [ ! -z "$TIDEWAYS_APP_NAME" ]; then
+if { [ -f /run/secrets/shared/tideways_api_key ] && [ ! -z "$TIDEWAYS_APP_NAME" ];}; then
     TIDEWAYS_API_KEY=$(cat /run/secrets/shared/tideways_api_key)
 
     echo "Tideways secret found"
