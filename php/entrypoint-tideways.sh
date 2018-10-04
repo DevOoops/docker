34104f548563956683f2bd52d9949f471b90ad9f
Kessler dev <kessler.dev@gmail.com>
2018-10-04 14:11:09 +0200

    Support also if no TIDEWAYS_APP_NAME env

--- php/entrypoint-tideways.sh
+++ php/entrypoint-tideways.sh
@@ -8,7 +8,7 @@ if [ -f /run/secrets/tideways_api_key ]; then
     echo "Tideways secret found"
 fi
 # if secret(k8s) is set
-if [ -f /run/secrets/shared/tideways_api_key ]; then
+if [ -f /run/secrets/shared/tideways_api_key ] && [ ! -z "$TIDEWAYS_APP_NAME" ]; then
     TIDEWAYS_API_KEY=$(cat /run/secrets/shared/tideways_api_key)
 
     echo "Tideways secret found"
@@ -24,7 +24,10 @@ if [ ! -z "$TIDEWAYS_API_KEY" ]; then
     echo "Tideways ENV $TIDEWAYS_ENV"
 
     # try to get rancher container name
-    TIDEWAYS_HOST=${TIDEWAY_HOST:-$(curl -s rancher-metadata/latest/self/container/name)}
+    if [ "$K8S_CONTEXT" = "" ]; then
+        TIDEWAYS_HOST=${TIDEWAY_HOST:-$(curl -s rancher-metadata/latest/self/container/name)}
+    fi
+
     if [ "$TIDEWAYS_HOST" = "" ]; then
         TIDEWAYS_HOST=$(cat /etc/hostname)
     fi
